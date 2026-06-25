0df930f419c71f04ec35171f600e1f9f50e42164
Ameed salameh <ameed.salame123@gmail.com>
2026-06-25 23:00:21 +0300

    up

---
+++ project-2/areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_2.Data;
+
+namespace project_2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
